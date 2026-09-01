44df8503b6257d2408948f944bb6162f39e1fd58
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:12:33 +0900

    7070600.0.0(7.7.6.0 Adapter)배포

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
-        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.5"),
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.6"),
     ],
     targets: [
         .target(name: "APSSPMediationVungle",
